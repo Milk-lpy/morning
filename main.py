07e5dff723abe4006b337ea0fb330f35141d0b63
Milk-lpy <81848987+Milk-lpy@users.noreply.github.com>
2022-08-22 13:51:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
+  words = requests.get("https://v.api.aa1.cn/api/tiangou/index.php")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
